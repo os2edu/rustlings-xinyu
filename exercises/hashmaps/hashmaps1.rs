9bff10832ab58b597ae013d078ed496cc0dc3030
Xinyu Zhou <i@sourcehut.net>
2022-11-04 19:47:49 +0800

    hashmaps1
    Signed-off-by: Xinyu Zhou <i@sourcehut.net>

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -10,19 +10,18 @@
 //
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket: HashMap<String, u32> = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
-
-    basket
+    basket.insert(String::from("apple"), 4);
+    basket.insert(String::from("mango"), 6);
+    return basket;
 }
 
 #[cfg(test)]
